6060875344cab7ae312e41bc08f7a165e248e474
MaxVoices <maksslesh@gmail.com>
2025-07-15 22:15:40 +0300

    Добавлена начальная структура проекта с логотипами, серверными скриптами и HTML-кодами
    - Созданы логотипы в формате SVG: main-logo-black.svg и main-logo-light.svg
    - Добавлен файл JavaScript: script.js
    - Реализованы серверные скрипты: auth.php, db.php, mail.php, saveLead.php и schema.sql
    - Добавлены PHP-файлы нижнего колонтитула и верхнего колонтитула для согласованного макета
    - Создан основной файл index.php и различные файлы разделов (о программе, контакты, сотрудничество, главная, проекты, команда)
    - Организована структура проекта в structure.txt

---
+++ includes/head.php
@@ -0,0 +1,16 @@
+<head>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-oENRd0BjEvS8S66s0UOD+xRX8+OdXy1R+y/L2WuY7T2n1R9B0+4yOo8mUzp7Hnx2" crossorigin="anonymous">
+
+  <!-- Font Awesome CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-pu7Q55hB8Vs9nQTFZn+cx9Rb1jhWf8Gr8SjdzOx+0kUuGV1QZQ8ZWXqP7GzqcuKPv7T7HYXpT6m7wW3/cxZ7Fw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+  <!-- jQuery (нужно до Bootstrap JS, если используешь Bootstrap 4) -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP3mvAz0X4QUK4DZ7qpQ6zG2UQ2yYUlHbBsJZz0iJVw=" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap JS Bundle (Popper включён) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-htSIrVqmItcF+0zRQ4AGr4KU7sJGT2Xp9mkNw7KfA3nyBjK1qPQzFKeD0bZXiJjN" crossorigin="anonymous"></script>
+
+  <!-- GSAP -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-xPRZXL6g4ZLU+bRGjTZs0x48R+qmhOMCddzO/LCv/RF0avUOeILAxPvDRTy2UN1qYVXyqwPE4c44FmvE1IMuWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
---
+++ includes/scripts.php
@@ -0,0 +1,8 @@
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP3mvAz0X4QUK4DZ7qpQ6zG2UQ2yYUlHbBsJZz0iJVw=" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap JS Bundle (Popper уже включён) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-htSIrVqmItcF+0zRQ4AGr4KU7sJGT2Xp9mkNw7KfA3nyBjK1qPQzFKeD0bZXiJjN" crossorigin="anonymous"></script>
+
+  <!-- GSAP -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-xPRZXL6g4ZLU+bRGjTZs0x48R+qmhOMCddzO/LCv/RF0avUOeILAxPvDRTy2UN1qYVXyqwPE4c44FmvE1IMuWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
