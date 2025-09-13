a4b0f128b38434e2a6ea82a097deb00eb26f57c3
MaxVoices <maksslesh@gmail.com>
2025-09-14 02:46:41 +0300

    feat: главная страница приветствия и локальные зависимости
    - Разработана и адаптирована главная страница приветствия «Добро пожаловать в Synlick»
    - Настроена структура для отображения контента на главной
    - Зависимости (Bootstrap, Font Awesome, GSAP, jQuery) скачаны в папку vendors
      для локального подключения вместо CDN
    - Обеспечено повышение скорости загрузки за счёт локальных ресурсов

--- includes/head.php
+++ includes/head.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
   <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  
+ 
+  
+  
+  
+  <!-- CSS проекта -->
+  <link rel="stylesheet" href="../assets/css/main.css">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-oENRd0BjEvS8S66s0UOD+xRX8+OdXy1R+y/L2WuY7T2n1R9B0+4yOo8mUzp7Hnx2" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
 
   <!-- Font Awesome CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-pu7Q55hB8Vs9nQTFZn+cx9Rb1jhWf8Gr8SjdzOx+0kUuGV1QZQ8ZWXqP7GzqcuKPv7T7HYXpT6m7wW3/cxZ7Fw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  
-  <!-- jQuery (нужно до Bootstrap JS, если используешь Bootstrap 4) -->
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP3mvAz0X4QUK4DZ7qpQ6zG2UQ2yYUlHbBsJZz0iJVw=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../assets/vendors/fontawesome/css/all.min.css">
 
-  <!-- Bootstrap JS Bundle (Popper включён) -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-htSIrVqmItcF+0zRQ4AGr4KU7sJGT2Xp9mkNw7KfA3nyBjK1qPQzFKeD0bZXiJjN" crossorigin="anonymous"></script>
+  <!-- jQuery -->
+  <script src="../assets/vendors/jquery/jquery.min.js"></script>
+
+  <!-- Bootstrap JS Bundle (включает Popper) -->
+  <script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- GSAP -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
-
-  <!-- CSS for header -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/CSSRulePlugin.min.js"></script>
+  <script src="../assets/vendors/gsap/gsap.min.js"></script>
+  <script src="../assets/vendors/gsap/CSSRulePlugin.min.js"></script>
 
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Test</title>
+  <title>Synclick</title>
 
 </head>
---
+++ sections/main.php
@@ -0,0 +1,11 @@
+<div class="containers main-page">
+    <div class="welcome-text">
+        <h1 class="name">Добро пожаловать в Synclick</h1>
+        <p class="description">
+            Команда разработчиков. Опытные профессионалы, 
+            которые помогут вам с вашим стартапом, придумают 
+            идею и сделают все так, как вам нравится
+        </p>
+        <button class="button-interesting">Interesting, what else?</button>
+    </div>
+</div>
