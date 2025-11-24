edc60a13de0d04a6e4309d4ad6f63bfe9a654120
MaxVoices <maksslesh@gmail.com>
2025-11-24 22:47:41 +0300

    Начало переписвание проекта на ReactVite

--- assets/js/everymain.js
+++
@@ -1,9 +0,0 @@
-const phoneInput = document.getElementById('phone');
-
-  const maskOptions = {
-    mask: '+{7}(000)000-00-00',
-    lazy: false,
-    placeholderChar: '_'
-  };
-
-  const mask = IMask(phoneInput, maskOptions);
--- includes/scripts.php
+++
@@ -1,4 +0,0 @@
-<script src="../assets/js/header.js"></script>
-<script src="../assets/js/about.js"></script>
-<script src="../assets/js/team.js"></script>
-<script src="../assets/js/everymain.js"></script>
