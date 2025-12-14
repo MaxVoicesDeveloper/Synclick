9b1dc1ac26d240afd40f5b4a986e818c96f6b140
maxvoices <maksslesh@gmail.com>
2025-12-14 23:17:14 +0300

    Создание компонента About

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainPage from './components/MainPage/MainPage.jsx'
+import About from './components/About/About.jsx'
 
 export default function App() {
   return (
     <>
       <MainPage />
+      <About />
     </>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import './about.modules.less/About.css'
+
+export default function About(){
+    return(
+        <>
+            <div className="containers about-page">
+                
+            </div>
+        </>
+    )
+}
