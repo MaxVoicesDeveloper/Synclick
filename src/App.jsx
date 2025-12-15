2f2e869fa5bb9d285931f40012be1aa78e01cd05
maxvoices <maksslesh@gmail.com>
2025-12-15 11:25:10 +0300

    Добавил фотографии и поменял пути у about экрана

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import MainPage from './components/MainPage/MainPage.jsx'
 import About from './components/About/About.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 export default function App() {
   return (
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,27 @@
-import './about.modules.less/About.css'
+import './About.css'
 
 export default function About(){
     return(
         <>
-            <div className="containers about-page">
-                
+            <div className="containers about-page" id="about">
+                <div className="about-text">
+                    <h1 className="name">About</h1>
+                    <p className="description">
+                        Development team. Experienced professionals who will help you with your startup,
+                        come up with an idea, and do everything as you like evelopment team.
+                        Experienced professionals who will help you with your startup, come up with an idea,
+                        and do everything as you like
+                    </p>
+                </div>
+                <div className="fragments-block">
+                    <div className="fragments">
+                        <div className="fragment so-big-os"><img src="../assets/images/components/about-out/os/so-big-os.png" alt="fragment"/></div>
+                        <div className="fragment big-os"><img src="../assets/images/components/about-out/os/big-os.png" alt="fragment"/></div>
+                        <div className="fragment large-os"><img src="../assets/images/components/about-out/os/large-os.png" alt="fragment"/></div>
+                        <div className="fragment small-os"><img src="../assets/images/components/about-out/os/small-os.png" alt="fragment"/></div>
+                        <div className="fragment so-small-os"><img src="../assets/images/components/about-out/os/so-small-os.png" alt="fragment"/></div>
+                    </div>
+                </div>
             </div>
         </>
     )
